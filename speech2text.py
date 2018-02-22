2a0808cdcd6e23fc0998c45cd12297bd85955330
rufaro katsamba <rufarokatsamba94@gmail.com>
2018-02-22 16:51:56 +0200

    initial commit

---
+++ speech2text.py
@@ -0,0 +1,22 @@
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, l am  just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("faffy just said " + r.recognize_google(audio))
+    with open("outputs.txt", "a") as f:
+        f.write("\n" + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
